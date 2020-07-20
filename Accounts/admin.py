4e6cafd6439f26ae55e9a3aacc03b29f48de8a2c
Shashwat Singh <shashwat5013@gmaiil.com>
2020-07-21 01:32:30 +0530

    tutorDetails migrated successfully shashwat

--- Accounts/admin.py
+++ Accounts/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import studentDetails
+from .models import studentDetails,tutorDetails
 # Register your models here.
 
 admin.site.register(studentDetails)
+admin.site.register(tutorDetails)
--- Accounts/models.py
+++ Accounts/models.py
@@ -1,11 +1,18 @@
 from django.db import models
-
 # Create your models here.
 
 class studentDetails(models.Model):
-    fullName = models.CharField(max_length=100)
-    emailId  = models.EmailField(max_length=254,primary_key=True)
-    userName = models.CharField(max_length=100)
-    password = models.CharField(max_length=10000)
-    profilePhoto = models.ImageField(upload_to='studentProfile')
+    fullName          = models.CharField(max_length=100)
+    emailId           = models.EmailField(max_length=254,primary_key=True)
+    userName          = models.CharField(max_length=100)
+    password          = models.CharField(max_length=10000)
+    profilePhoto      = models.ImageField(upload_to='studentProfile')
     termsAndCondition = models.BooleanField(default=False)
+
+class tutorDetails(models.Model):
+    fullName         = models.CharField(max_length=100)
+    emailId          = models.EmailField(max_length=254,primary_key=True)
+    gender           = models.CharField(max_length=10)
+    mobileNumber     = models.CharField(max_length=20)
+    specialityCourse = models.CharField(max_length=50)
+    profilePhoto     = models.ImageField(upload_to='tutorProfile')
